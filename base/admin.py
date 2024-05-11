9b2d77d742f74b0a4f699953f03f35c084b6d7a9
Rohit Thakre <thake785@gmail.com>
2024-05-11 22:14:14 +0530

    tenant added.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from base.models import College, Student
+
+admin.site.register(College)
+admin.site.register(Student)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django_multitenant import fields as tenant_fields
+from django_multitenant import models as tenant_models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class College(tenant_models.TenantModel):
+    tenant_id = "id"
+    name = models.CharField(
+        help_text="name of the college",
+        max_length=200,
+        unique=True,
+    )
+    slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
+
+
+    is_active = models.BooleanField(
+        help_text="identify if the organisation is active currently or not",
+        default=True,
+    )
+   
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Student(tenant_models.TenantModel):
+    tenant_id = "college_id"
+    college = models.ForeignKey(College, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
